19f10cc4260178c5ef5f01f8b378d9cfb0b3143e
Michael Rice <mrice@redhat.com>
2015-10-09 15:25:36 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+        <meta charset="utf-8">
+        <title>LA Software Craftsmanship</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+        <p><strong>LA Software Craftsmanship</strong></p>
+
+        <p><em>Join us</em> at <a href="http://www.meetup.com/LA-Software-Craftsmanship/">http://www.meetup.com/LA-Software-Craftsmanship/</a></p>
+
+
+        <p>
+                <strong>Community notes</strong><br>
+                The <a href="http://scna.softwarecraftsmanship.org/"></a><em>Software Craftsmanship North America</em></a> is coming to Los Angeles on November 6, 2015.
+        </p>
+
+        <p>
+                <strong>Slack</strong>
+                <em>Join us on Slack</em> <a href='https://softwarecraftsmanship.slack.com/archives/los-angeles'>here</a> (contact Michael below for an invite).
+        </p>
+
+
+        <p>
+                <strong>Reading list (in no particular order)</strong><br>
+                <ul>
+                        <li>8th Light blog [<a href="https://blog.8thlight.com/">https://blog.8thlight.com/</a>]</li>
+                        <li>Code Climate blog [<a href="http://blog.codeclimate.com/">http://blog.codeclimate.com/</a>]</li>
+                        <li>Codurance bloc [<a href="http://codurance.com/blog/">http://codurance.com/blog/</a>]</li>
+                        <li>Martin Fowler: <a href="http://martinfowler.com/bliki/CraftmanshipAndTheCrevasse.html">CraftmanshipAndTheCrevasse</a></li>
+                        <li>//TODO: add more!!</li>
+                </ul>
+        </p>
+
+        <p>
+                Questions, concerns, comments? Contact the organization, Michael Rice, at <a href="mailto:me@michaelrice.com">me@michaelrice.com</a>.
+        </p>
+
+</body>
+</html>
+
